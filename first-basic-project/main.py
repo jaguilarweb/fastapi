f5d430aa0f1d4c3271029af44ba6d1a92b75d4b8
Jenny Aguilar <contact@jaguilarweb.com>
2026-04-25 22:47:09 +0200

    feat: add customer data type

--- first-basic-project/main.py
+++ first-basic-project/main.py
@@ -1,6 +1,15 @@
 import zoneinfo
-from fastapi import FastAPI
 from datetime import datetime
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Customer(BaseModel):
+    name: str
+    description: str | None
+    email: str
+    age: int
+
 
 app = FastAPI()
 
@@ -28,3 +37,9 @@ async def time2(iso_code: str):
     timezone_str = country_timezones.get(iso)
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"time": datetime.now(tz)}
+
+
+@app.post("/customers")
+async def create_customer(customer_data: Customer):
+    return customer_data
+
