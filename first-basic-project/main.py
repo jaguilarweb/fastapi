e24ecad160c4e26c8e89b743787608cc0ff980ca
Jenny Aguilar <contact@jaguilarweb.com>
2026-04-25 22:30:05 +0200

    refactor: timezone

--- first-basic-project/main.py
+++ first-basic-project/main.py
@@ -1,4 +1,6 @@
+import zoneinfo
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
@@ -6,4 +8,23 @@ app = FastAPI()
 async def root():
     return{"message" : "hello world"}
 
+country_timezones = {
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
 
+
+@app.get("/time")
+async def time():
+    return {"time": datetime.now()}
+
+
+@app.get("/time2/{iso_code}")
+async def time2(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    return {"time": datetime.now(tz)}
