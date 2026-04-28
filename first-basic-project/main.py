ea86dd6d231829483a1a14812607576ae0c4bf89
Jenny Aguilar <contact@jaguilarweb.com>
2026-04-28 17:40:12 +0200

    feat: add Customer and Customer list endopoint with db configuration

--- first-basic-project/main.py
+++ first-basic-project/main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from fastapi import FastAPI
 from models import Customer, Transaction, Invoice, CustomerCreate, Transaction, Invoice
 from db import SessionDep, create_all_tables
+from sqlmodel import select 
 
 
 app = FastAPI(lifespan=create_all_tables)
@@ -33,7 +34,7 @@ async def time2(iso_code: str):
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"time": datetime.now(tz)}
 
-# Se cre un afunción de id, mientras no se tenga base de datos.
+# Se crea una función de id, mientras no se tenga base de datos.
 db_customers: list[Customer] = []
 
 # Con customerCreate estamos recibiendo datos, sin id.
@@ -41,14 +42,15 @@ db_customers: list[Customer] = []
 @app.post("/customers", response_model=Customer)
 async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     customer = Customer.model_validate(customer_data.model_dump()) # Validamos los datos recibidos
-    # Asumiendo que lo hace la base de datos (fake).
-    customer.id = len(db_customers)
-    db_customers.append(customer)
+    session.add(customer)
+    session.commit()
+    session.refresh(customer)
     return customer
 
 @app.get("/customers", response_model=list[Customer])
-async def list_customers():
-    return db_customers
+async def list_customers(session: SessionDep):
+    return session.exec(select(Customer)).all()
+
 
 
 @app.get("/customers/{customer_id}", response_model=Customer)
