920e0fe141e075a9544cdd8234542c25b51571fb
Jenny Aguilar <contact@jaguilarweb.com>
2026-04-30 15:21:38 +0200

    feat: add delete and patch customers endpoints

--- first-basic-project/main.py
+++ first-basic-project/main.py
@@ -1,8 +1,8 @@
 import zoneinfo
 from datetime import datetime
 
-from fastapi import FastAPI
-from models import Customer, Transaction, Invoice, CustomerCreate, Transaction, Invoice
+from fastapi import FastAPI, HTTPException, status
+from models import Customer, Transaction, Invoice, CustomerCreate, CustomerUpdate, Transaction, Invoice
 from db import SessionDep, create_all_tables
 from sqlmodel import select 
 
@@ -34,11 +34,19 @@ async def time2(iso_code: str):
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"time": datetime.now(tz)}
 
+# --------------------
+# Customers
+#---------------------
+
+
 # Se crea una función de id, mientras no se tenga base de datos.
 db_customers: list[Customer] = []
 
+
 # Con customerCreate estamos recibiendo datos, sin id.
 # Con response_model=Customer devolvemos el modelo Customer, con id.
+
+# CREATE
 @app.post("/customers", response_model=Customer)
 async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     customer = Customer.model_validate(customer_data.model_dump()) # Validamos los datos recibidos
@@ -47,18 +55,49 @@ async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     session.refresh(customer)
     return customer
 
+# GET ALL
 @app.get("/customers", response_model=list[Customer])
 async def list_customers(session: SessionDep):
     return session.exec(select(Customer)).all()
 
-
-
+# GET BY ID
 @app.get("/customers/{customer_id}", response_model=Customer)
-async def get_customer(customer_id: int):
-    for customer in db_customers:
-        if customer.id == customer_id:
-            return customer
-    return {"error": "Customer not found"}
+async def read_customer(customer_id: int, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if customer_db:
+        return customer_db
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+
+# DELETE
+@app.delete("/customers/{customer_id}")
+async def delete_customer(customer_id: int, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if customer_db:
+        session.delete(customer_db)
+        session.commit()
+        return {"message": "Customer deleted successfully"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+
+# PATCH
+@app.patch("/customers/{customer_id}", response_model=Customer, status_code=status.HTTP_201_CREATED)
+async def edit_customer(customer_id: int, customer_data: CustomerUpdate , session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Customer doesn't exist"
+        )
+    
+    customer_data_dict = customer_data.model_dump(exclude_unset=True) # Obtenemos un diccionario con los campos que se recibieron
+    customer_db.sqlmodel_update(customer_data_dict) # Actualizamos solo los campos que se recibieron
+    session.add(customer_db)
+    session.commit()
+    session.refresh(customer_db) # Actualiza valor en memoria
+    return customer_db
+
+
+
+
+
 
 
 @app.post("/transactions")
